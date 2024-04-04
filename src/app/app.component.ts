49b51291637f3bb6f066637c14c5f1ceaca96505
nurlykhan.kakpan <nurlykhan.kakpan@hardcode.kz>
2024-04-04 08:45:21 +0500

    I am strating learn git and Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'MyFirstNgApp';
+  purchase : string = '';
+  price : number = 0;
+
+  items : Item[] = [
+    new Item('Bread',15.6),
+    new Item('Meat',130.5),
+    new Item('Rice', 12.4)
+  ];
+
+  addItem(purchase : string, price : number) : void{
+    if(purchase === '' || purchase === null){
+      alert('You need write purchase name')
+      return;
+    }
+    this.items.push(new Item(purchase,price));
+    this.purchase = '';
+    this.price = 0;
+  }
+}
+
+
+class Item {
+  purchase: string;
+  done : boolean;
+  price : number
+
+  constructor(purchase : string, price : number ) {
+    this.purchase = purchase;
+    this.price = price;
+    this.done = false;
+  }
 }
